7a5de5738d8e2b0f326df92a35e4cb76280944b4
Kévin Subileau <github@kevinsubileau.fr>
2013-11-06 13:19:43 +0100

    Add class autoloader.

--- api/index.php
+++ api/index.php
@@ -2,12 +2,13 @@
 
 require( './config.php' );
 
-/** Chemin absolu vers le dossier de WordPress. */
+/** Chemin absolu vers le dossier de ZZChat. */
 if ( !defined('ABSPATH') )
     define('ABSPATH', dirname(__FILE__) . '/');
 
 require( ABSPATH . '/support/default-constants.php' );
 require( ABSPATH . '/support/helpers.php' );
+require( ABSPATH . '/support/ClassLoader.php' );
 require( ABSPATH . '/vendor/Slim/Slim.php' );
 
 // ZZChat calculates offsets from UTC.
@@ -19,8 +20,17 @@ set_initial_constants( );
 // Set debug mode.
 set_debug_mode( );
 
-// Register Slim auto loader.
+// Register Slim's auto loader.
 \Slim\Slim::registerAutoloader();
+
+// Register non-Slim autoloader
+ClassLoader::addDirectories(array(
+    ABSPATH.'/controllers',
+    ABSPATH.'/models',
+    ABSPATH.'/routers',
+    ABSPATH.'/support',
+));
+ClassLoader::register();
 
 $app = new \Slim\Slim(array(
     'debug' => ZC_DEBUG,
