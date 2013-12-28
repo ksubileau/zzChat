46dea782b3a1cef3ccf9d5252a29acf10b10e114
Kévin Subileau <github@kevinsubileau.fr>
2013-12-28 21:31:29 +0100

    Move PHP tests into a new subdirectory.

--- tests/PHPUnit/bootstrap.php
+++ tests/PHPUnit/bootstrap.php
@@ -2,10 +2,10 @@
 
 define('ZZCHAT_TEST_MODE', true);
 
-set_include_path(dirname(__FILE__) . '/../api' . PATH_SEPARATOR . get_include_path());
+set_include_path(dirname(__FILE__) . '/../../api' . PATH_SEPARATOR . get_include_path());
 
 if ( !defined('ABSPATH') )
-    define('ABSPATH', dirname(__FILE__) . '/../api');
+    define('ABSPATH', dirname(__FILE__) . '/../../api');
 
 if ( !defined('TESTABSPATH') )
     define('TESTABSPATH', dirname(__FILE__));
